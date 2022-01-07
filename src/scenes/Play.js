046721a63bb7f78aa90f0f99ce3887734bb9e4a2
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2022-01-07 07:40:06 -0300

    62. Layer Refactor

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,18 +1,26 @@
 import Preload from "./Preload"
 
 class Play extends Phaser.Scene {
-    constructor () {
+    constructor() {
         super('PlayScene')
     }
 
     create() {
-        const map = this.make.tilemap({key: 'map'})
-        const tileset1 = map.addTilesetImage('main_lev_build_1', 'tile-1')
-        // const tileset2 = map.addTilesetImage('main_lev_build_2', 'tile-2')
+        const map = this.createMap()
+        const layers = this.createLayers(map)
+    }
 
-        
-        map.createStaticLayer('platforms', tileset1)
-        map.createStaticLayer('environment', tileset1)
+    createMap() {
+        const map = this.make.tilemap({ key: 'map' })
+        map.addTilesetImage('main_lev_build_1', 'tile-1')
+        return map
+    }
+
+    createLayers(map) {
+        const tileset = map.getTileset('main_lev_build_1')
+        const platforms = map.createStaticLayer('platforms', tileset)
+        const environment = map.createStaticLayer('environment', tileset)
+        return { platforms, environment }
     }
 
 }
