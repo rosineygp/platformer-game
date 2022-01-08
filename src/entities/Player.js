f2dbd0126eca0dcb345642c3bf1b49205a21c9ed
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2022-01-08 19:07:10 -0300

    69. Player class preupdate

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -6,6 +6,35 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
         scene.add.existing(this)
         scene.physics.add.existing(this)
+
+        this.init()
+    }
+
+    init() {
+        this.gravity = 500
+        this.speed = 200
+
+        this.cursors = this.scene.input.keyboard.createCursorKeys()
+
+        this.body.setGravityY(500)
+        this.setCollideWorldBounds(true)
+
+    }
+
+    preUpdate() {
+        const { left, right, space } = this.cursors
+
+        if (left.isDown) {
+            this.setVelocityX(-this.speed)
+        } else if (right.isDown) {
+            this.setVelocityX(this.speed)
+        } else {
+            this.setVelocityX(0)
+        }
+
+        if (space.isDown) {
+            this.setVelocityY(-this.speed)
+        }
     }
 }
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,12 +10,9 @@ class Play extends Phaser.Scene {
         const map = this.createMap()
         const layers = this.createLayers(map)
 
-        this.playerSpeed = 200
-        this.player = this.createPlayer()
+        const player = this.createPlayer()
 
-        this.physics.add.collider(this.player, layers.platformColliders)
-
-        this.cursors = this.input.keyboard.createCursorKeys()
+        this.physics.add.collider(player, layers.platformColliders)   
 
     }
 
@@ -38,26 +35,7 @@ class Play extends Phaser.Scene {
     }
 
     createPlayer() {
-        const player = new Player(this, 100, 250)
-        player.body.setGravityY(500)
-        player.setCollideWorldBounds(true)
-        return player
-    }
-
-    update() {
-        const { left, right, space} = this.cursors
-
-        if (left.isDown) {
-            this.player.setVelocityX(-this.playerSpeed)
-        } else if (right.isDown) {
-            this.player.setVelocityX(this.playerSpeed)
-        } else {
-            this.player.setVelocityX(0)
-        }
-
-        if (space.isDown) {
-            this.player.setVelocityY(-this.playerSpeed)
-        }
+        return new Player(this, 100, 250)
     }
 
 }
